513b391a1c17686eb46c3e697739ed738468f291
Ankit Kumar <57948013+ankitsynx@users.noreply.github.com>
2023-12-02 21:11:53 +0530

    Create boot.py
    yes

---
+++ micropython/projects/pot_watering/boot.py
@@ -0,0 +1,52 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+import uos, machine
+import sys
+import gc
+import webrepl
+import time
+import network
+import esp
+import BlynkLib
+
+## WiFi Config
+ssid = 'Ankit'
+password = 'Compaq890#'
+
+print("Connecting to Wifi Network: ",ssid)
+## Wifi Connection
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connected to Wifi network: ',ssid)
+print(station.ifconfig())
+
+## WebREPL run
+webrepl.start()
+gc.collect()
+sys.path.reverse()
+# Disable debug output to avoid issues with Webrepl
+esp.osdebug(None)
+
+
+##Blynk
+# Fill-in information from Blynk Device Info here 
+BLYNK_TEMPLATE_ID = "TMPL3ocj5gXoX"
+BLYNK_TEMPLATE_NAME = "Quickstart Template"
+BLYNK_AUTH_TOKEN = "sjWj7u-LOoovHvqSgWmB-Pu5wh6efwbz"
+
+blynk = BlynkLib.Blynk(BLYNK_AUTH_TOKEN)
+
+@blynk.on("connected")
+def blynk_connected(ping):
+    print('Blynk ready. Ping:', ping, 'ms')
+
+@blynk.on("disconnected")
+def blynk_disconnected():
+    print('Blynk disconnected')
+    
+    
+
